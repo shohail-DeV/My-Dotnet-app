c6343b232213200594c5062759b2a922bc55b0ea
Shaikh Shohail Parwej <shohail.parwej@corp.csmpl.com>
2026-01-06 18:13:51 +0530

    fix jenkinsfile 6

--- obj/Release/net8.0/MyApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e6462205431a9fedca56e586185392b24a2c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
