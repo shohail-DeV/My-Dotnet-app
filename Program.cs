a2f2fe4e1cba538f3c6adbaebef059107d7c114d
Shohail Parwej <102964720+shohail-DeV@users.noreply.github.com>
2026-01-20 15:52:16 +0530

    Update greeting message in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! This is .NET 8 Deployment");
+app.MapGet("/", () => "Hello World! This is a demonstration of .NET 8 Deployment");
 
 app.Run();
